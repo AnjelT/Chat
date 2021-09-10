1041582c09db1186aee18592c8074063dcb4e6bf
AnjelT <anjela.tumanyan.y@tumo.org>
2021-09-10 20:20:47 +0400

    added roomName and userList

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
-const chatForm = document.getElementById("chat-form")
-const chatMessages = document.querySelector(".chat-messages")
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 //Get username and room from URL --?
 const {username,room} = Qs.parse(location.search,{
@@ -55,5 +57,24 @@ function outputMessage(message){
 
 //Add room name to DOM
 function outputRoomName(room){
-  
+  roomName.innerText = room;
 }
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
+
+//Prompt the user before leave chat room
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
